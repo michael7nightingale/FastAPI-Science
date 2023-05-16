0d75e919cf47425f3177e32b43d8530a73eefd55
Michael Nightingale <122199262+michael7nightingale@users.noreply.github.com>
2023-05-16 08:39:24 +0500

    Update main.py

--- internal/main.py
+++ internal/main.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
-from app.internal.users import get_current_user
+from internal.users import get_current_user
 
 
 main_router = APIRouter(
