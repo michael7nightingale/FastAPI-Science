c8bc958d0846fdce5fc460dd048ff6f5e738d0b6
Michael Nightigale <suslanchikmopl@gmail.com>
2023-05-16 18:25:36 +0500

    updated 16.05.2023

--- configuration/logger.py
+++ configuration/logger.py
@@ -1,9 +1,12 @@
 import logging
-# import os
+import os
 
 
 def create_logger():
+    print(123, os.getcwd())
     logger = logging.Logger(name='app_loger', level='DEBUG')
+    if not os.path.exists(os.getcwd() + '/log/'):
+        os.mkdir(os.getcwd() + '/log/')
     file_handler = logging.FileHandler(filename='log/app.log')
     formatter = logging.Formatter("%(asctime)s   |   %(levelname)s   |   %(message)s")
     file_handler.setFormatter(formatter)
