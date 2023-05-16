c50cf2aa826b707b3833b10a7cc32d7a4adedfa1
Michael Nightingale <122199262+michael7nightingale@users.noreply.github.com>
2023-05-16 08:46:25 +0500

    Update database.py

--- package/database.py
+++ package/database.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.hash import django_pbkdf2_sha256
-from app.package import schema
+
+from package import schema
 
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
