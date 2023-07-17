182dff3853c5b76259a38ae66746a49143dca53a
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-17 22:30:08 +0500

    make science refactoring

--- app/models/schemas/sciences.py
+++ app/models/schemas/sciences.py
@@ -4,8 +4,12 @@ from enum import Enum
 
 class RequestSchema(BaseModel):
     data: dict
-    user: object = None
+    url: str
+    method: str = "GET"
     result: str | None = None
+    find_mark: str = "x"
+    user_id: str | None
+    nums_comma: int = 10
 
 
 class ScienceEnum(Enum):
