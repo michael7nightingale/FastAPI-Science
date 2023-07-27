1e6e28806f0e23f19b95ec57880b2578436ec16f
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-27 23:49:32 +0500

    delete sockets dependencies

--- app/app/dependencies/__init__.py
+++ app/app/dependencies/__init__.py
@@ -1,6 +1,5 @@
 from fastapi_authtools.exceptions import raise_permissions_error
 
-from .database import get_repository, get_socket_repository
 from .auth import get_user_register_data
 from .sciences import get_all_sciences
 from .cabinets import get_table_filepath
--- app/app/dependencies/database.py
+++ app/app/dependencies/database.py
@@ -17,32 +17,6 @@ async def _get_session(pool=Depends(_get_pool)):
         yield session
 
 
-def _get_socket_pool(func):
-    settings = get_app_settings()
-    engine = create_async_engine(settings.db_uri)
-    pool = async_sessionmaker(bind=engine)
-
-    def inner(pool_=pool, *args, **kwargs):
-        return func(pool_=pool_, *args, **kwargs)
-
-    return inner
-
-
-@_get_socket_pool
-async def _get_socket_session(pool_: async_sessionmaker):
-    async with pool_() as session:
-        yield session
-
-
-def get_socket_repository(repo_type: Type[BaseRepository]):
-    """Get repository instance after getting the session."""
-
-    def _get_repo(session=_get_socket_session) -> BaseRepository:
-        return repo_type(next(session()))
-
-    return _get_repo
-
-
 def get_repository(repo_type: Type[BaseRepository]):
     """Get repository instance after getting the session."""
     def _get_repo(session=Depends(_get_session)) -> BaseRepository:
