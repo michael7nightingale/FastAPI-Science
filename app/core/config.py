cef33f534de6f6d70c042864c9a8b809e8769b07
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-27 23:45:59 +0500

    refactor environment variables and app config

--- app/core/config.py
+++ app/core/config.py
@@ -1,43 +1,67 @@
-import os
-
 from pydantic_settings import BaseSettings
+import os
 from socket import gethostbyname
 
 
-class Settings(BaseSettings):
-    db_user: str
-    db_password: str
-    db_driver: str
-    db_host: str
-    db_port: str
-    db_name: str
+class BaseAppSettings(BaseSettings):
+    SUPERUSER_USERNAME: str
+    SUPERUSER_PASSWORD: str
+    SUPERUSER_EMAIL: str
 
-    superuser_username: str
-    superuser_password: str
-    superuser_email: str
+    ALGORITHM: str
+    SECRET_KEY: str
+    EXPIRE_MINUTES: int
 
-    algorithm: str
-    secret_key: str
-    expire_minutes: int
+    github_redirect_url: str = "https://github.com/michael7nightingale/Calculations-FastAPI"
+    github_client_id: str = "asdasd"
+
+    @property
+    def github_login_url(self) -> str:
+        return f"https://github.com/login/oauth/authorize?client_id={self.github_client_id}"
+
+
+class DevSettings(BaseAppSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_DRIVER: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+
+    @property
+    def db_uri(self) -> str:
+        host_address = gethostbyname(self.DB_HOST)
+        return f"{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{host_address}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+        if os.getenv("DOCKER"):
+            env_file = ".test.env"
+        else:
+            env_file = ".dev.env"
+
+
+def get_app_settings() -> DevSettings:
+    return DevSettings()
+
+
+class TestSettings(BaseAppSettings):
+    DB_DRIVER: str
+    DB_NAME: str
 
     github_redirect_url: str = "https://github.com/michael7nightingale/Calculations-FastAPI"
     github_client_id: str = "asdasd"
 
     @property
     def db_uri(self) -> str:
-        host_address = gethostbyname(self.db_host)
-        return f"{self.db_driver}://{self.db_user}:{self.db_password}@{host_address}:{self.db_port}/{self.db_name}"
+        return f"{self.DB_DRIVER}:///{self.DB_NAME}"
 
     @property
     def github_login_url(self) -> str:
         return f"https://github.com/login/oauth/authorize?client_id={self.github_client_id}"
 
     class Config:
-        if os.getenv("DOCKER"):
-            env_file = ".docker.env"
-        else:
-            env_file = ".dev.env"
+        env_file = ".test.env"
 
 
-def get_app_settings() -> Settings:
-    return Settings()
+def get_test_app_settings() -> TestSettings:
+    return TestSettings()
