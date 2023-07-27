e87950f5a0efff7f6b4e6982beecd7f4ac564016
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-27 11:14:52 +0500

    delete repository classes files

--- app/db/repositories/cabinets.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.db.repositories.base import BaseRepository
-from app.db.models import History
-
-
-class HistoryRepository(BaseRepository):
-    def __init__(self, session: AsyncSession):
-        super().__init__(History, session)
