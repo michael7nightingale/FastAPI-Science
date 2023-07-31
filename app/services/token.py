a8ced69d7c201dc7302d49843c5eb133941b85eb
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-31 11:40:39 +0500

    add activation token logic

---
+++ app/services/token.py
@@ -0,0 +1,29 @@
+from itsdangerous import URLSafeTimedSerializer
+
+from app.core.config import get_app_settings
+
+
+def generate_token(email):
+    secret_key = get_app_settings().SECRET_KEY
+    serializer = URLSafeTimedSerializer(secret_key=secret_key)
+    return serializer.dumps(email)
+
+
+def confirm_token(token, expiration=3600):
+    secret_key = get_app_settings().SECRET_KEY
+    serializer = URLSafeTimedSerializer(secret_key=secret_key)
+    try:
+        email = serializer.loads(
+            token, max_age=expiration
+        )
+        return email
+    except Exception:
+        return False
+
+
+def generate_activation_link(request, user) -> str:
+    token = generate_token(user.email)
+    url = request.app.url_path_for("activate_user", uuid=user.id, token=token)
+    # host = request.client.host
+    host = "http://127.0.0.1:8000"
+    return host + url
