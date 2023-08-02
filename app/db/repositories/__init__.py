9d9460f611f5d3ee20caab778c5852cba153c6af
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-02 23:33:07 +0500

    delete unnecessary files

--- app/db/repositories/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base import BaseRepository
-from .sqlalchemy_async import SQLAlchemyAsyncRepository
--- app/db/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sciences import ScienceService, CategoryService, FormulaService
-from .users import UserService
-from .cabinets import HistoryService
-from .problems import ProblemService, ProblemMediaService, SolutionService, SolutionMediaService
