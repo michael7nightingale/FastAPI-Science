9172c3bb845e38c6a993d981962891cf3974e09d
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-13 13:00:52 +0500

    create problem pydantic models

--- src/apps/problems/schemas.py
+++ src/apps/problems/schemas.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel
 
 
+class ProblemCreate(BaseModel):
+    title: str
+    text: str
+    science_id: str
+
+
+class ProblemUpdate(BaseModel):
+    title: str | None = None
+    text: str | None = None
+    science: str | None = None
+
+
 class SolutionCreate(BaseModel):
     text: str
