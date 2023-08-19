f7fc9cfb0339e9112730cdc427b7152509156f67
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-19 14:30:32 +0500

    create footer component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    name: "FooterItem",
+    data(){
+      return{
+        author: "michael7nightingale",
+      }
+    },
+    computed:{
+      year() {
+        let now = new Date();
+        return now.getFullYear();
+      }
+    }
+  }
+</script>
+
+<template>
+<footer class="footer_">
+      <p class="">© {{ year }}, Все права защищены. Создан {{ author }}</p>
+  </footer>
+</template>
+
+<style>
+
+</style>
