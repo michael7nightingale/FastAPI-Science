e6ba85b1fce76cd2f613fe0ad0243deccc598033
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-19 14:30:05 +0500

    initialize Vue.js project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">сайт для вычислений</h5>
+        <router-link to="/">
+          <img :src="`${publicPath}images/logo.png`" class="nav-logo" width="17%" height="10%">
+        </router-link>
+
+        <nav class="my-2 my-md-0 mr-md-4">
+            <a href="{{ editMeUrl }}">
+                Edit me
+                <img alt="" :src="`${publicPath}images/github.png`" class="nav-logo" width=10% height="10%">
+            </a>
+
+            <router-link class="p-2 text-dark" to="/">Home</router-link>
+           <router-link class="p-2 text-dark" to="/auth/login">Login</router-link>
+        </nav>
+    </div>
+
+  <div class="body-content background"  >
+    <hr/>
+    <router-view/>
+  </div>
+
+  <Footer/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+.background{
+        //background-image: url("");
+        min-height: 700px;
+}
+.white_text{
+  color: whitesmoke;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
+
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "AppItem",
+  components: {
+    Footer,
+
+  },
+  data(){
+    return {
+      publicPath: process.env.BASE_URL,
+      editMeUrl: 'https://github.com/michael7nightingale/FastAPI-Science',
+
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/main/HomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+
+  <div class="jumbotron">
+      <h1>Сайт для физико-математических вычислений</h1>
+  </div>
+
+  <div class="row">
+      <div class="block col-md-4">
+          <h2>Науки и формулы</h2>
+          <p>
+             счет по формулам
+          </p>
+          <router-link class="btn btn-outline-primary text" to="/sciences">перейти &raquo;</router-link>
+      </div>
+      <div class="block col-md-4">
+          <h2>Задачи и решения</h2>
+          <p>
+             Помогайте другим разваваться и развивайтесь сами
+          </p>
+          <router-link class="btn btn-outline-primary text" to="/problems">перейти &raquo;</router-link>
+      </div>
+  </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
+
+
+<style>
+.block{
+  background-color: #ececec;
+  padding: 20px;
+  border-radius: 10px;
+  margin-left: 5%;
+
+}
+
+</style>
