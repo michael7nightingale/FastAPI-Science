c53f199d179b72e76c08236c890d33009590a91d
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-21 01:21:59 +0500

    add global css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -76,32 +76,5 @@ export default {
 </template>
 
 <style>
-.background{
-        background-image: url("/public/images/main_bg.jpg");
-        min-height: 700px;
-  padding-bottom: 100px;
-}
-.white_text{
-  color: whitesmoke;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import './assets/css/global.css';
 </style>
