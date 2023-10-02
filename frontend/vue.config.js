6d4be86328c2dc65eee26b2ece586a105964fc1f
michael7nightingale <suslanchikmopl@gmail.com>
2023-10-02 22:38:03 +0500

    create vue https config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = {
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8880, // CHANGE YOUR PORT HERE!
+    https: true,
+  },
+}
