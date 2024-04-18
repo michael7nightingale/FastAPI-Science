d370cb70ee053cc09869562652e0a1b9b58fcdd6
michael7nightingale <suslanchikmopl@gmail.com>
2024-04-18 21:35:16 +0500

    refactoring

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import HomeView from '@/views/main/HomeView.vue';
 import LoginView from "@/views/users/LoginView.vue";
 import RegisterView from "@/views/users/RegisterView.vue";
@@ -155,7 +155,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes
 })
 
